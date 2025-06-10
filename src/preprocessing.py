9901a16ed7db4170e3caf089504a0d4681cce060
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-06-10 02:08:36 +0200

    added output

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def preprocess_data(data):
+def preprocess_data(train_data: pd.DataFrame, test_data: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Preprocess the data
     """
