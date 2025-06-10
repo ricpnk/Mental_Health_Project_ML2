a9e67c158cdbb923c2395227271f8b17eb9e22b5
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-06-10 02:07:02 +0200

    implemented load_data function

--- src/load_data.py
+++ src/load_data.py
@@ -4,4 +4,6 @@ def load_data():
     """
     Load data from a csv file
     """
-    raise NotImplementedError("load_data method not implemented")
+    train_data = pd.read_csv("data/train.csv")
+    test_data = pd.read_csv("data/test.csv")
+    return train_data, test_data
