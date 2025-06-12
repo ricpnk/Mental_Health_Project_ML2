aa7cff592e028d26642ed5f5b581a2f00c51e3c5
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-06-12 02:20:34 +0200

    added device support + minor changes / now trainable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import datetime
+import torch
 from src.load_data import load_data
-from src.model import MLPClassifier, train_model, evaluate_model, save_model
+from src.model import MLPClassifier, train, evaluate, save_model
 from src.preprocessing import preprocess_data
 
 # Define constants
@@ -9,12 +10,12 @@ TIMESTAMP = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 MODELPATH = f"saved_models/model{TIMESTAMP}.pth"
 
 # Define Hyperparameters
-INPUT_DIM = 0
-HIDDEN_DIM = 0
-OUTPUT_DIM = 0
-EPOCHS = 0
-LEARNING_RATE = 0
-BATCH_SIZE = 0
+HIDDEN_DIM = 128
+OUTPUT_DIM = 2
+DROPOUT = 0.3
+EPOCHS = 50
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 32
 
 
 
@@ -28,25 +29,36 @@ def main():
     """
     # Sicherstellen, dass der Ordner für Modelle existieren
     os.makedirs("saved_models", exist_ok=True)
+
+    # Setzen des Geräts (CPU oder GPU)
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.backends.mps.is_available():
+        device = 'mps'
+    else:
+        device = 'cpu'
+    print(f"Using device: {device}")
     
     # Daten laden
-    train_data, test_data = load_data()
+    data = load_data()
 
     # Daten aufbereiten
-    processed_train_data, processed_test_data = preprocess_data(train_data, test_data)
+    train_data, test_data = preprocess_data(data, BATCH_SIZE)
+
+    # set input dimension with number of features
+    INPUT_DIM = next(iter(train_data))[0].shape[1]  # Anzahl der Features nach der Vorverarbeitung
+
 
     # Model initialisieren
-    model = MLPClassifier(INPUT_DIM, HIDDEN_DIM, OUTPUT_DIM)
+    model = MLPClassifier(INPUT_DIM, HIDDEN_DIM, DROPOUT, OUTPUT_DIM).to(device)
 
     # Modell trainieren
-    train_model(model, processed_train_data)
+    model, accuracy = train(model, train_data, test_data, EPOCHS, LEARNING_RATE, device)
     save_model(model, MODELPATH)
-    print("Model saved as trained_model.pth")
+    print("Model saved to ", MODELPATH)
 
-    # Modell evaluieren
-    evaluation_results = evaluate_model(processed_test_data, model)
 
-    print("Evaluation Results:", evaluation_results)
+    print("Evaluation Results:", accuracy)
 
 
 if __name__ == "__main__":
