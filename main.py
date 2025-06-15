290de18a22c1d100dbf700568419a88c4b7d57ed
ricopenkalski <51368644+ricopenkalski@users.noreply.github.com>
2025-06-15 08:26:23 +0200

    commenting

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ DIRECTORY = f"saved_models/model_{TIMESTAMP}"
 MODELPATH = f"saved_models/model_{TIMESTAMP}/best_model.pth"
 
 # Define Hyperparameters
-HIDDEN_DIM = 256
+HIDDEN_DIM = 128
 OUTPUT_DIM = 2
-DROPOUT = 0.4
+DROPOUT = 0.5
 EPOCHS = 50
 LEARNING_RATE = 0.0005
 BATCH_SIZE = 64
-NUM_HIDDEN_LAYERS = 5
+NUM_HIDDEN_LAYERS = 10
 
 
 
 def main():
     """
-    Arbeitsplan:
-    - Daten laden
-    - Daten aufbereiten
-    - Modell trainieren
-    - Modell evaluieren
+    main function to train the model
+    - set device
+    - load data
+    - preprocess data
+    - train model + evaluate
     """
-    # Sicherstellen, dass der Ordner für Modelle existieren
+    # create directory for models
     os.makedirs(DIRECTORY, exist_ok=True)
 
-    # Setzen des Geräts (CPU oder GPU)
+    # set device
     if torch.cuda.is_available():
         device = 'cuda'
     elif torch.backends.mps.is_available():
@@ -41,23 +41,21 @@ def main():
         device = 'cpu'
     print(f"Using device: {device}")
     
-    # Daten laden
+    # load data
     data = load_data()
 
-    # Daten aufbereiten
+    # preprocess data
     train_data, test_data = preprocess_data(data, BATCH_SIZE)
 
     # set input dimension with number of features
     INPUT_DIM = next(iter(train_data))[0].shape[1]
 
-    # Model initialisieren
+    # initialize model
     model = MLPClassifier(INPUT_DIM, HIDDEN_DIM, DROPOUT, OUTPUT_DIM, NUM_HIDDEN_LAYERS).to(device)
 
-    # Modell trainieren
-    model, accuracy = train(model, train_data, test_data, MODELPATH, EPOCHS, LEARNING_RATE, device)
+    # train model + evaluate
+    model = train(model, train_data, test_data, MODELPATH, EPOCHS, LEARNING_RATE, device)
 
-    # Modell evaluieren
-    print("Evaluation Results:", accuracy)
 
 
 if __name__ == "__main__":
